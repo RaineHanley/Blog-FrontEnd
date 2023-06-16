33766475c34b9be153f48b4eadcfc07a279d997e
RaineHanley <rainehanley@gmail.com>
2023-06-16 08:52:41 -0700

    everything is back

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Header() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <header>
+      <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">N</a>
+    </header>
+  );
 }
 
-export default App
+function PostsNew() {
+  return (
+    <div id="posts-new">
+      <h1>New post</h1>
+      <form>
+        <div>
+          Title: <input type="text" />
+        </div>
+        Image: <input type="text" />
+        <div>
+          Content: <input type="text" />
+        </div>
+        <button type="submit">Create post</button>
+      </form>
+    </div>
+  );
+}
+
+function PostsIndex(props) {
+  return (
+    <div id="posts-index">
+      <h1>All posts</h1>
+      {props.posts.map((post) => (
+        <div className="posts" key={post.id}>
+          <h2> {post.title}</h2>
+          <img src={post.image_url} alt="" />
+          <p>Author: Raine Hanley</p>
+          <button>More info</button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function Content() {
+  const [posts, setPosts] = useState([11]);
+
+  const handleIndexPosts = () => {
+    axios.get("http://localhost:3000/posts.json").then((response) => {
+      console.log(response.data);
+      setPosts(response.data);
+    });
+  };
+
+  useEffect(handleIndexPosts, []);
+
+  return (
+    <div>
+      <PostsNew />
+      <button onClick={handleIndexPosts}>Load Posts</button>
+      <PostsIndex posts={posts} />
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright 2022</p>
+    </footer>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
